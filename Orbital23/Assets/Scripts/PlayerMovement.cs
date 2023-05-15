9e0b3714d4af6b69a147351fd0b781bcfaabe004
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-05-15 18:57:18 +0800

    Implemented simple mouse-follow script for Racket object

---
+++ Orbital23/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+// using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float moveSpeed = 10f;
+    Rigidbody2D rb;
+    Vector2 position = new Vector2(0f, 0f);
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(position);
+    }
+}
