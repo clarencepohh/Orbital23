f480053220ee4e0fbc869d85bb0bb70e4d5f592c
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-05-17 15:57:12 +0800

    Removed outdated code

--- Orbital23/Assets/Scripts/ShuttlecockMovement.cs
+++ Orbital23/Assets/Scripts/ShuttlecockMovement.cs
@@ -4,7 +4,6 @@ public class ShuttlecockMovement : MonoBehaviour
 {
     private Rigidbody2D rb; 
     public Transform centerOfGravity;
-    // public Transform rotationTarget;
     public float rotationSpeed = 1.0f;
 
     void Start()
@@ -17,14 +16,7 @@ public class ShuttlecockMovement : MonoBehaviour
     }
 
     private void Update() {
-        // Vector3 targetDirection = rb.velocity;
-        // float oneStep = rotationSpeed * Time.deltaTime;
-        // Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, oneStep, 0.0f);
-        // transform.rotation = Quaternion.LookRotation(newDirection);
         Vector3 targetDirection = rb.velocity;
-        // float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-        // Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        // rb.rotation = Quaternion.Slerp(rb.rotation, q, Time.deltaTime * rotationSpeed); 
         if (targetDirection != Vector3.zero)
         {
             Quaternion toRotate = Quaternion.LookRotation(Vector3.forward, targetDirection);
