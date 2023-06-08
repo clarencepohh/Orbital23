4df3452218fcb3b6805b149ec482ec274c09fb79
ChenKangg <e0969211@u.nus.edu>
2023-06-08 23:06:03 +0800

    edit item spawn, obstacles

--- Orbital23/Assets/Scripts/Spawning/SpawnMagnet.cs
+++ Orbital23/Assets/Scripts/Spawning/SpawnMagnet.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnTest : MonoBehaviour
+public class SpawnMagnet : MonoBehaviour
 {
-    public GameObject Coin, Heart;
+    public GameObject Object;
     public float maxX;
     public float minX;
     public float maxY;
     public float minY;
-    public float TimebetSpawn;
-    private float SpawnTime; 
-   // public float amtobj;
+    public float TimeBetweenSpawn;
+    private float SpawnTime;
 
     void Update()
     {
         if(Time.time > SpawnTime)
         {
             Spawn();
-            SpawnTime = Time.time + TimebetSpawn;
+            SpawnTime = Time.time + TimeBetweenSpawn;
         }
     }
 
@@ -27,7 +26,6 @@ public class SpawnTest : MonoBehaviour
         float X = Random.Range(minX, maxX);
         float Y = Random.Range(minY, maxY);
 
-        Instantiate(Coin, transform.position + new Vector3(X, Y, 0), transform.rotation);
-        Instantiate(Heart, transform.position + new Vector3(X, Y, 0), transform.rotation);
+        Instantiate(Object, transform.position + new Vector3(X, Y, 0), transform.rotation);
     }
 }
