9f0ae578c948d1c15150dd3b71a3e0b138449a76
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-06-19 17:01:44 +0800

    Implemented test get/put request for leaderboard
    RestClient for unity

--- Orbital23/Assets/PlayerScores.cs
+++ Orbital23/Assets/PlayerScores.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Random = System.Random;
+using Proyecto26;
+using TMPro;    
 public class PlayerScores : MonoBehaviour
 {
-    public Text scoreText;
-    public InputField nameText;
+    public TextMeshProUGUI scoreText;
+    public TMP_InputField nameText;
     private Random random = new Random();
-    public int playerScore;
-    public string playerName;
+    public static int playerScore;
+    public static string playerName;
+    User user = new User();
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,37 @@ public class PlayerScores : MonoBehaviour
     public void OnSubmit()
     {
         playerName = nameText.text;
+        PostToDatabase();
     }
-    // Update is called once per frame
-    void Update()
+
+    public void GetScore()
     {
-        
+        GetFromDatabase();
+    }
+
+    private void UpdateScore()
+    {
+        if (user == null)
+        {
+            scoreText.text = "No such user";
+        } 
+        else 
+        {
+            scoreText.text = "Score: " + user.userScore;
+        }
+    }
+    private void PostToDatabase()
+    {   
+        User user = new User();
+        RestClient.Put("https://orbital23-coc-default-rtdb.asia-southeast1.firebasedatabase.app/" + playerName + ".json", user);
+    }
+
+    private void GetFromDatabase()
+    {
+        RestClient.Get<User>("https://orbital23-coc-default-rtdb.asia-southeast1.firebasedatabase.app/" + nameText.text + ".json").Then(response => 
+        {
+            user = response;
+            UpdateScore();
+        });
     }
 }
