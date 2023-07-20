643ff229be368afc7e72ad663a0cf901973bbe3f
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-07-20 14:54:15 +0800

    Added click SFX for menu buttons

--- Orbital23/Assets/Scripts/Leaderboard/EndScreenManager.cs
+++ Orbital23/Assets/Scripts/Leaderboard/EndScreenManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -5,10 +6,25 @@ public class EndScreenManager : MonoBehaviour
 {
     private int endScore;
     public TextMeshProUGUI scoreText;
+    private AudioSource clickSoundEffect;
 
     private void Start() 
     {
         endScore = ItemCollector.score;    
         scoreText.text = "Your score is: " + endScore + "\nClick to submit to leaderboard\n";
+    }
+    public IEnumerator buttonAudioClick()
+    {   
+        bool done = false;
+        clickSoundEffect = GetComponent<AudioSource>();
+        clickSoundEffect.Play();
+        done = true;
+        yield return new WaitWhile(() => done == false);
+    } 
+
+    public void onPressAudio()
+    {
+        StartCoroutine(buttonAudioClick());
+        new WaitForSeconds(0.1f);
     }
 }
